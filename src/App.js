7b0929f208b73c2c89ee335bfb085800b13cffed
Zac <zacharyflynn06@gmail.com>
2021-07-13 12:36:19 -0600

    commit tree 933488f64269ca7b8243dc2fa9098bde49bc3205

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    state = {  }
+
+
+    componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json")
+        .then(res => res.json)
+        .then(data => {
+            this.setState({
+              peopleInSpace: data.people
+            })
+        })
+    }
+    
+    render() { 
+        return ( 
+            <div>
+
+            </div>
+         );
+    }
+}
+ 
+export default App;
